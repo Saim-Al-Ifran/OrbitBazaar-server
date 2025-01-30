2c6a0bc49b8eb54ee3341e008442b0b8b8287854
Saim Al Ifran <phpmysql811@gmail.com>
2025-01-30 20:07:07 +0600

    added routes for deleting all wishlist data

--- dist/routes/wishlist/wishlist.route.js
+++ dist/routes/wishlist/wishlist.route.js
@@ -9,6 +9,6 @@ const wishlist_controller_1 = require("../../controllers/wishlist/wishlist.contr
 const router = express_1.default.Router();
 router.post("/wishlist", authenticate_1.default, wishlist_controller_1.addToWishlist);
 router.get("/wishlist", authenticate_1.default, wishlist_controller_1.getAllWishlist);
-router.delete("/wishlist/:productId", authenticate_1.default);
-router.delete("/wishlist", authenticate_1.default);
+router.delete("/wishlist/:productId", authenticate_1.default, wishlist_controller_1.removeProductFromWishlist);
+router.delete("/wishlist", authenticate_1.default, wishlist_controller_1.removeAllProductFromWishlist);
 exports.default = router;
--- src/routes/wishlist/wishlist.route.ts
+++ src/routes/wishlist/wishlist.route.ts
@@ -1,13 +1,18 @@
 import express from 'express';
 import authenticate from '../../middlewares/auth/authenticate';
-import { addToWishlist, getAllWishlist } from '../../controllers/wishlist/wishlist.controller';
+import {
+    addToWishlist,
+    getAllWishlist,
+    removeAllProductFromWishlist,
+    removeProductFromWishlist
+} from '../../controllers/wishlist/wishlist.controller';
    
 const router = express.Router();
 
  
 router.post("/wishlist",  authenticate, addToWishlist);
 router.get("/wishlist",  authenticate, getAllWishlist);
-router.delete("/wishlist/:productId",  authenticate,);
-router.delete("/wishlist",  authenticate,);
+router.delete("/wishlist/:productId",  authenticate, removeProductFromWishlist);
+router.delete("/wishlist",  authenticate, removeAllProductFromWishlist);
 
 export default router;
