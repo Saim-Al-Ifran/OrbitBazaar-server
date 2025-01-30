253b3890fb841da6cc3821b46d298aaa8088f749
Saim Al Ifran <phpmysql811@gmail.com>
2025-01-30 22:20:57 +0600

    created Report schema and interface

---
+++ src/models/Report.ts
@@ -0,0 +1,34 @@
+import mongoose, { Schema } from 'mongoose';
+import { IReport } from '../types/models/Report';
+ 
+const ReportSchema = new Schema<IReport>(
+  {
+    productID: {
+        type: Schema.Types.ObjectId,
+        ref: 'Product',
+        required: true
+    },
+    userEmail: {
+        type: String,
+        required: true
+    },
+    reason: {
+        type: String,
+        required: true
+    },
+    comment: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: String,
+        num: ['pending', 'resolve', 'reject'],
+        default: 'pending'
+    },
+  },
+  { timestamps: true }
+);
+
+ 
+const Report = mongoose.model<IReport>('Report', ReportSchema);
+export default Report;
---
+++ src/types/models/Report.d.ts
@@ -0,0 +1,10 @@
+import   Document from 'mongoose';
+interface IReport extends Document {
+    productID: mongoose.Types.ObjectId;
+    userEmail: string;
+    reason: string;
+    comment: string;
+    status: 'pending' | 'resolve' | 'reject';
+    createdAt: Date;
+    updatedAt: Date;
+  }
