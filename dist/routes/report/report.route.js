778330dbb5caafd76f2c48d699b583f779c71812
Saim Al Ifran <phpmysql811@gmail.com>
2025-02-04 20:54:05 +0600

    modify swagger.yaml for report modules

--- dist/routes/report/report.route.js
+++ dist/routes/report/report.route.js
@@ -9,17 +9,11 @@ const report_controller_1 = require("../../controllers/report/report.controller"
 const authorizeVendor_1 = __importDefault(require("../../middlewares/auth/authorizeVendor"));
 const router = express_1.default.Router();
 router.post('/reports', authenticate_1.default, report_controller_1.addReport);
-// Get reports by vendor (Authenticated vendor)
 router.get('/reports/vendor', authenticate_1.default, authorizeVendor_1.default, report_controller_1.getReportsByVendor);
 router.get("/reports/vendor/report/:reportId", authenticate_1.default, authorizeVendor_1.default, report_controller_1.getReportByIdForVendor);
-// Get a specific report by ID (Authenticated users)
 router.get('/reports/user/:reportId', authenticate_1.default, report_controller_1.getSingleReportByUser);
-// Delete a report (Authenticated users or vendors)
 router.delete('/reports/user/:reportId', authenticate_1.default, report_controller_1.removeReportByUser);
-// Update a report (Authenticated users, only if status is not resolved/rejected)
 router.put('/reports/user/:reportId', authenticate_1.default, report_controller_1.editReportByUser);
-// Get all reports submitted by the authenticated user
 router.get('/reports/user', authenticate_1.default, report_controller_1.getReportsByUser);
-// Update report status (Authenticated vendors only)
 router.patch('/reports/vendor/:reportId/status', authenticate_1.default, authorizeVendor_1.default, report_controller_1.editReportStatus);
 exports.default = router;
--- src/routes/report/report.route.ts
+++ src/routes/report/report.route.ts
@@ -16,24 +16,12 @@ import authorizeVendor from '../../middlewares/auth/authorizeVendor';
 const router = express.Router();
 
 router.post('/reports', authenticate, addReport);
-// Get reports by vendor (Authenticated vendor)
 router.get('/reports/vendor',authenticate,authorizeVendor,getReportsByVendor);
-
 router.get("/reports/vendor/report/:reportId", authenticate,authorizeVendor, getReportByIdForVendor  );
-
-// Get a specific report by ID (Authenticated users)
 router.get('/reports/user/:reportId', authenticate, getSingleReportByUser );
-
-// Delete a report (Authenticated users or vendors)
 router.delete('/reports/user/:reportId', authenticate, removeReportByUser);
-
-// Update a report (Authenticated users, only if status is not resolved/rejected)
 router.put('/reports/user/:reportId',authenticate , editReportByUser);
-
-// Get all reports submitted by the authenticated user
 router.get('/reports/user', authenticate, getReportsByUser);
-
-// Update report status (Authenticated vendors only)
 router.patch('/reports/vendor/:reportId/status', authenticate , authorizeVendor , editReportStatus  );
 
 
